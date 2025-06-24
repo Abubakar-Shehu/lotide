ad06e4bec25e7b70404d710e8277edc11f560569
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-24 17:56:30 -0600

    changed the conditional statement after the c-style loop to first check for negatives so that if it is positive it keeps checking

--- eqArrays.js
+++ eqArrays.js
@@ -9,12 +9,12 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1, array2) {
   if(array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]){
-        //console.log(array1[i], array2[i])
-        return true
-      } else {
+      if (array1[i] !== array2[i]){
         //console.log(array1[i], array2[i])
         return false
+      } else {
+        //console.log(array1[i], array2[i])
+        return true
       }
     }
   }
