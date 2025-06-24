f65ab68df5bd3a39f3e61c7eb308a1a86bef2b6d
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-24 17:49:26 -0600

    added functionality to the function

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if(array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]){
+        //console.log(array1[i], array2[i])
+        return true
+      } else {
+        //console.log(array1[i], array2[i])
+        return false
+      }
+    }
+  }
+  return false
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
