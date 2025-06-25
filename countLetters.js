4851a532121e2dedb09aef11ac741c71320f2ced
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-25 15:55:01 -0600

    eslinted it

--- countLetters.js
+++ countLetters.js
@@ -6,31 +6,31 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function(string){
+const countLetters = function(string) {
   let result = {};
   
   const noSpaces = string.split(" ").join("");
-  const arrayOfLetter = noSpaces.split("")
+  const arrayOfLetter = noSpaces.split("");
   let individualLetter = [];
   // Want to iterate over the string to count how many letters there are
   // Need to count how many each letter are but also be able to know what letters they are
   for (const letters of arrayOfLetter) {
-    if(!individualLetter.includes(letters)){
-      individualLetter.push(letters)
+    if (!individualLetter.includes(letters)) {
+      individualLetter.push(letters);
     }
   }
 
-  for(const lets of individualLetter) {
+  for (const lets of individualLetter) {
     let count = 0;
     for (const amo of arrayOfLetter) {
-      if (lets === amo){
-        count++
+      if (lets === amo) {
+        count++;
       }
     }
-    result[lets] = count
+    result[lets] = count;
   }
-  return result
+  return result;
 };
 
-console.log(countLetters("lighthouse in the house"))
-console.log(countLetters('LHL'))
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters('LHL'));
