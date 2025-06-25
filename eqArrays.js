de9f6fb668b005d90ce144088b1a35c40fb4b693
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-24 18:00:22 -0600

    refactored to a desired level for now

--- eqArrays.js
+++ eqArrays.js
@@ -10,21 +10,17 @@ const eqArrays = function(array1, array2) {
   if(array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]){
-        //console.log(array1[i], array2[i])
         return false
-      } else {
-        //console.log(array1[i], array2[i])
-        return true
-      }
+      } 
     }
   }
-  return false
+  return true
 }
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
-//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
