1b8b3d7a5efa65946a703a65bc2b4c0476331436
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-25 17:52:27 -0600

    refactored eqArrays and assert. Added functionality to letter with help from pedro

--- eqArrays.js
+++ eqArrays.js
@@ -7,16 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
+  if (array1.length !== array2.length) {
     return false;
   }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
@@ -26,3 +26,5 @@ eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for(let i = 0; i < sentence.length; i++){
+    const letter = sentence[i];
+   
+    if(letter !== ' ' && results[letter]) {
+      results[letter].push(i)
+    } else if (letter !== ' '){
+      results[letter] = [i]
+    }
+  }
+
+  return results;
+};
+
+
+
+console.log(letterPositions("lighthouse in the house"))
