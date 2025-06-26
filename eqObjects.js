d15d81b6aa814a32ae3a3592e52df03167bd4188
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-25 19:11:06 -0600

    added functionality for arrays

--- eqObjects.js
+++ eqObjects.js
@@ -27,10 +27,13 @@ const eqObjects = function(object1, object2) {
   }
 
   for (const val in object1) {
-    if (object1[val] !== object2[val]) {
+    if(Array.isArray(object1[val])){
+      eqArrays(object1[val],object2[val])
+    }else if (object1[val] !== object2[val]) {
       return false;
     }
     return true;
+
   }
 
 };
