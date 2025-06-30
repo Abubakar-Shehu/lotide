3e5e6f92e363e49ee78758d06972e67770fcb188
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-30 16:54:00 -0600

    added the flatten and map due to compass

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-// const flatten = require('./flatten');
+const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-// const map = require('./map');
+const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -29,9 +29,9 @@ module.exports = {
   eqObjects,
   findKey,
   findKeyByValue,
-  // flatten: flatten,
+  flatten,
   letterPositions,
-  // map: map,
+  map,
   takeUntil,
   without
 };
