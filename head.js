4bdf9027ba8cfaf5233160868c996d47bdc3eba7
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-06-30 14:12:32 -0600

    refactored to export module rather than copy paste it in the code and also added a test file to test the functions.

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
@@ -14,7 +6,4 @@ const head = function(arr) {
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 3);
-assertEqual(head([]), 0);
+module.exports = head;
